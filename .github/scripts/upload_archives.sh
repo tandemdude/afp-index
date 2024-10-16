33b056bab49848799559d0c38fe4d22848aca6cf
tandemdude <43570299+tandemdude@users.noreply.github.com>
2024-10-16 13:50:06 +0100

    feat: update CI to prevent a second release if one already exists for that version

--- .github/scripts/upload_archives.sh
+++ .github/scripts/upload_archives.sh
@@ -12,15 +12,6 @@ for archive in archives/*.zip; do
 
   gh release upload $TAG_NAME "$archive#$(basename $archive)" \
     || (echo "Archive $(basename archive) upload failed" && exit 1)
-#
-#  curl -L \
-#    -X POST \
-#    -H "Accept: application/vnd.github+json" \
-#    -H "Authorization: token $GITHUB_TOKEN" \
-#    -H "X-GitHub-Api-Version: 2022-11-28" \
-#    -H "Content-Type: application/zip" \
-#    $UPLOAD_URL?name=$(basename $archive) \
-#    --data-binary "@$archive" || (echo "Archive $(basename archive) upload failed" && exit 1)
 
   # Increment the counter
   ((counter++))
@@ -31,6 +22,10 @@ for archive in archives/*.zip; do
     echo "Uploaded $counter files. Sleeping for 80 seconds..."
     sleep 80
   fi
+
+  if (( counter % 400 == 0)); then
+    echo "Sleeping an additional hour due to hourly rate-limit..."
+    sleep 4200
 done
 
 echo "All files have been uploaded!"
